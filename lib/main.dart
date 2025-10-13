5be91d0f6522d506acc58aaf7c74a390ef6b9cda
ChristianMarcellino <christianmarcellino14@gmail.com>
2025-10-13 20:24:40 +0700

    SDSD

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       useMaterial3: true,
       ),
-      home: const detailScreen(candi: candiList[0]),
+      home: detailScreen(placeholder: candiList[0]),
     );
   }
 }
--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'package:wisata_candi/models/candi.dart';
 class detailScreen extends StatelessWidget {
-  const detailScreen({super.key});
 
+  final Candi placeholder;
+
+  const detailScreen({super.key, required this.placeholder});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
      body: Column(
       children: [
         Stack(
@@ -14,14 +16,83 @@ class detailScreen extends StatelessWidget {
             child: ClipRRect(
               borderRadius: BorderRadius.circular(20),
               child: Image.asset(
-                candi.ImageAsset,
+                placeholder.imageAsset,
                 width: double.infinity,
                 height: 300,
                 fit: BoxFit.cover,
               ),
-            ),)
+            ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple[100]?.withOpacity(0.8),
+                  shape: BoxShape.circle,
+                ),
+                child: IconButton(onPressed: (){}, 
+                icon: Icon(Icons.arrow_back)
+                ),
+              ),
+            ),
           ],
-        )
+        ),
+        Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                children: [
+                  SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(placeholder.name),
+                      IconButton(onPressed: (){}, 
+                      icon: Icon(Icons.favorite_border)
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  Row(
+                    children: [
+                      Icon(Icons.place, color: Colors.red,),
+                      SizedBox(width: 8,),
+                      SizedBox(width: 78,
+                        child: Text('Lokasi', style: TextStyle(
+                          fontWeight: FontWeight.bold
+                        ),
+                        ),
+                      ),
+                      Text(": ${placeholder.location}",)
+                    ],),
+                  Row(
+                    children: [
+                      Icon(Icons.calendar_month, color: Colors.red,),
+                      SizedBox(width: 8,),
+                      SizedBox(width: 78,
+                        child: Text('Dibangun', style: TextStyle(
+                          fontWeight: FontWeight.bold
+                        ),
+                        ),
+                      ),
+                      Text(": ${placeholder.built}",)
+                    ],
+                    ),
+                  Row(
+                    children: [
+                      Icon(Icons.house, color: Colors.red,),
+                      SizedBox(width: 8,),
+                      SizedBox(width: 78,
+                        child: Text('Tipe', style: TextStyle(
+                          fontWeight: FontWeight.bold
+                        ),
+                        ),
+                      ),
+                      Text(": ${placeholder.type}",)
+                    ],
+                    ),
+                ],
+              ),
+              )
       ],
      ), 
     );
