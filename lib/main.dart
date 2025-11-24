847d8e7e7cceb91872dcefe95a0787b9ccd9e0f7
MichaelJoyClement <christianmarcellino14@gmail.com>
2025-11-24 18:33:33 +0700

    dadlosakd;lsa

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/screen/detail_screen.dart';
 import 'package:wisata_candi/data/candi_data.dart';
+import 'package:wisata_candi/screen/home_screen.dart';
 import 'package:wisata_candi/screen/screen.dart';
 import 'package:wisata_candi/screen/sign_in_screen.dart';
 void main() {
@@ -19,11 +20,7 @@ class MyApp extends StatelessWidget {
       colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       useMaterial3: true,
       ),
-      home: Scaffold(
-        body : Center(
-          child : Text("Wawan")
-        )
-      ), 
+      home: HomeScreen(), 
     );
   }
 }
---
+++ lib/widget/item_card.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:wisata_candi/models/candi.dart';
+import 'package:wisata_candi/screen/detail_screen.dart';
+
+class ItemCard extends StatelessWidget {
+  final Candi candi;
+  const ItemCard({super.key, required this.candi});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context, 
+          MaterialPageRoute(builder: (context) => DetailScreen(placeholder: candi))
+        );
+      },
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        margin: EdgeInsets.all(4),
+        elevation: 1,
+        child: Column(
+          children: [
+            Expanded(
+              child: ClipRRect(
+                child: Image.asset(
+                  candi.imageAsset, 
+                width: double.infinity,
+                fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top : 8.0, left:16),
+              child: Text(candi.name, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom : 8.0, left:16),
+              child: Text(candi.type, style: TextStyle(fontSize: 12),),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
